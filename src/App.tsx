29a9ae2b1d099495496a2e17c4bc36f7d5fb5172
Edilson Rodrigues <edilsonrodrigues.ciandt@bexsbanco.com.br>
2023-01-26 19:09:58 -0300

    feat: added feature toggles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Feature, FeatureToggles } from "@paralleldrive/react-feature-toggles";
+
+const features = ["@active-component", "@inactive-component"];
+const featureName = "@active-component";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <FeatureToggles features={features}>
+          <Feature
+            name={featureName}
+            inactiveComponent={() => <p>This is the inactive component</p>}
+            activeComponent={() => <p>This is the active component</p>}
+          />
+        </FeatureToggles>
       </header>
     </div>
   );
