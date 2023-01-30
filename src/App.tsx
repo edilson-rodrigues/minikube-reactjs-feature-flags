3df4c045b58b15e24129ac6528feea00ba720c86
Edilson Rodrigues <edilsonrodrigues.ciandt@bexsbanco.com.br>
2023-01-30 19:20:54 -0300

    feat:

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,46 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Feature, FeatureToggles } from "@paralleldrive/react-feature-toggles";
 import { useFeatureToggle } from "./hooks";
+import { client } from "./client";
 
 const featureName = "@active-component";
 
 function App() {
-  const { isFeatureEnabled } = useFeatureToggle();
+  const { flags, getFlags } = useFeatureToggle();
+
+  const handleChangeFlag = async (flag: string) => {
+    client
+      .patch("flag/1", {
+        flag,
+      })
+      .then(() => getFlags())
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <FeatureToggles features={isFeatureEnabled}>
+        <FeatureToggles features={flags}>
           <Feature
             name={featureName}
-            inactiveComponent={() => <p>This is the inactive component</p>}
-            activeComponent={() => <p>This is the active component</p>}
+            inactiveComponent={() => (
+              <div>
+                <p>This is the inactive component</p>
+                <button onClick={() => handleChangeFlag("@active-component")}>
+                  active component
+                </button>
+              </div>
+            )}
+            activeComponent={() => (
+              <div>
+                <p>This is the active component</p>
+                <button onClick={() => handleChangeFlag("@inactive-component")}>
+                  inactive component
+                </button>
+              </div>
+            )}
           />
         </FeatureToggles>
       </header>
