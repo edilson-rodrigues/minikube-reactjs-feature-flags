dc3c87d4c411099d9a94c8ba01627f23b5e03c79
Edilson Rodrigues <edilsonrodrigues.ciandt@bexsbanco.com.br>
2023-01-30 19:20:05 -0300

    feat: added get flags in use feature toggles

--- src/hooks/useFeatureToggle.tsx
+++ src/hooks/useFeatureToggle.tsx
@@ -1,7 +1,31 @@
-export function useFeatureToggle() {
-  const features = ["@active-component", "@inactive-component"];
+import { useEffect, useState } from "react";
+import { client } from "../client";
+import { FeatureFlagsData, FeatureToggle, Flags } from "./types";
+
+export function useFeatureToggle(): FeatureToggle {
+  const [data, setData] = useState<FeatureFlagsData>();
+
+  async function getFlags() {
+    client
+      .get("flags")
+      .then((response) => setData(response.data))
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+    if (!data) {
+      getFlags();
+    }
+
+    return;
+  }, [data]);
+
+  const flags: Flags = data
+    ? data?.data.map((feature) => feature.flag)
+    : ([] as Flags);
 
   return {
-    isFeatureEnabled: features,
+    flags,
+    getFlags,
   };
 }
